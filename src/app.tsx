4877afcf31dfcefed72235048981b42792b0c721
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-05 20:04:27 -0300

    :sparkles: feat(form): add uncontrolled and controlled components example

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,34 @@
+import { useRef, useState } from 'react'
+
 export function App() {
+  const [age, setAge] = useState('')
+
+  const inputNameRef = useRef<HTMLInputElement>(null)
+
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    console.log({
+      age,
+      name: inputNameRef.current?.value,
+    })
+  }
+
+  console.log('rendered')
+
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <h1 className="font-bold text-4xl">Hello, World!</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+        <input type="text" ref={inputNameRef} />
+
+        <input
+          type="number"
+          value={age}
+          onChange={(event) => setAge(event.target.value)}
+        />
+
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   )
 }
