9ff3e2508eceec4af097881fbbb2ae8eb2763f91
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-06 02:04:57 -0300

    :sparkles: feat(form): use react-hook-form for form handling

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,38 @@
-import { useRef, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 export function App() {
-  const [age, setAge] = useState('')
+  const { handleSubmit: submit, register } = useForm()
 
-  const inputNameRef = useRef<HTMLInputElement>(null)
+  /**
+   * O handleSubmit é uma função currying: que retorna outra função.
+   * No caso, ele retorna: (event?: React.BaseSyntheticEvent) => Promise<unknown>
+   *
+   * Ele recebe como argumento uma função chamada onValid, que é a função
+   * callback que será executada quando todos os dados do formulário estiverem
+   * válidos.
+   */
 
-  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
-    event.preventDefault()
+  const handleSubmit = submit((data) => {
+    console.log('Formulário submetido')
 
-    console.log({
-      age,
-      name: inputNameRef.current?.value,
-    })
-  }
+    console.log({ data })
+  })
 
   console.log('rendered')
+
+  /**
+   * O useForm também retorna a função register, que serve para registrar
+   * o input
+   */
+
+  // const inputName = form.register('name')
 
   return (
     <div className="flex min-h-screen items-center justify-center">
       <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-        <input type="text" ref={inputNameRef} />
+        <input type="text" {...register('name')} />
 
-        <input
-          type="number"
-          value={age}
-          onChange={(event) => setAge(event.target.value)}
-        />
+        <input type="number" {...register('age')} />
 
         <button type="submit">Enviar</button>
       </form>
