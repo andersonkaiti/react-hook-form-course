9978d0403c7b4ac4d39beb004d4c1e6d96807932
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-06 03:02:18 -0300

    :sparkles: feat(validation): display per-field error messages

--- src/app.tsx
+++ src/app.tsx
@@ -1,37 +1,66 @@
 import { useForm } from 'react-hook-form'
 
 export function App() {
-  const { handleSubmit: submit, register } = useForm()
+  const { handleSubmit: submit, register, formState } = useForm()
 
-  const handleSubmit = submit(
-    (data) => {
-      console.log({ data })
-    },
-    (errors) => {
-      console.log({ errors })
-    },
-  )
+  const handleSubmit = submit((data) => {
+    console.log({ data })
+  })
+
+  // reativo
+  console.log(formState.errors)
 
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-        <input
-          type="text"
-          {...register('name', {
-            required: true,
-            minLength: 2,
-          })}
-        />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <div>
+          <input
+            type="text"
+            {...register('name', {
+              required: {
+                value: true,
+                message: 'Este campo é obrigatório',
+              },
+              minLength: {
+                value: 2,
+                message: 'Tem que ter no mínimo 2 dígitos',
+              },
+            })}
+          />
 
-        <input
-          type="number"
-          {...register('age', {
-            required: true,
-            min: 18,
-            max: 99,
-            setValueAs: (value) => Number(value),
-          })}
-        />
+          {formState.errors.name && (
+            <small className="block text-red-400">
+              {formState.errors.name?.message}
+            </small>
+          )}
+        </div>
+
+        <div>
+          <input
+            type="number"
+            {...register('age', {
+              required: {
+                value: true,
+                message: 'Este campo é obrigatório',
+              },
+              min: {
+                value: 18,
+                message: 'A idade mínima é de 18 anos',
+              },
+              max: {
+                value: 99,
+                message: 'A idade máxima é de 99 anos',
+              },
+              setValueAs: (value) => Number(value),
+            })}
+          />
+
+          {formState.errors.age && (
+            <small className="block text-red-400">
+              {formState.errors.age.message}
+            </small>
+          )}
+        </div>
 
         <button type="submit">Enviar</button>
       </form>
