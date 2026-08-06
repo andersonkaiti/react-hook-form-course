7fed5468efa45d42146c2dd10687665e0a110ef0
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-06 02:37:17 -0300

    :sparkles: feat(validation): add field validation and error handler

--- src/app.tsx
+++ src/app.tsx
@@ -3,36 +3,35 @@ import { useForm } from 'react-hook-form'
 export function App() {
   const { handleSubmit: submit, register } = useForm()
 
-  /**
-   * O handleSubmit é uma função currying: que retorna outra função.
-   * No caso, ele retorna: (event?: React.BaseSyntheticEvent) => Promise<unknown>
-   *
-   * Ele recebe como argumento uma função chamada onValid, que é a função
-   * callback que será executada quando todos os dados do formulário estiverem
-   * válidos.
-   */
-
-  const handleSubmit = submit((data) => {
-    console.log('Formulário submetido')
-
-    console.log({ data })
-  })
-
-  console.log('rendered')
-
-  /**
-   * O useForm também retorna a função register, que serve para registrar
-   * o input
-   */
-
-  // const inputName = form.register('name')
+  const handleSubmit = submit(
+    (data) => {
+      console.log({ data })
+    },
+    (errors) => {
+      console.log({ errors })
+    },
+  )
 
   return (
     <div className="flex min-h-screen items-center justify-center">
       <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-        <input type="text" {...register('name')} />
+        <input
+          type="text"
+          {...register('name', {
+            required: true,
+            minLength: 2,
+          })}
+        />
 
-        <input type="number" {...register('age')} />
+        <input
+          type="number"
+          {...register('age', {
+            required: true,
+            min: 18,
+            max: 99,
+            setValueAs: (value) => Number(value),
+          })}
+        />
 
         <button type="submit">Enviar</button>
       </form>
